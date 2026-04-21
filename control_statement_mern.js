96c038149841d4c60a3c62593c6840aab7d98753
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-04-21 15:47:21 +0530

    day4

--- control_statement_mern.js
+++ control_statement_mern.js
@@ -1,3 +1,5 @@
+
+//IF-ELSE
 var num = 11;
 
 // if(num%2 == 0){
@@ -8,3 +10,17 @@ var num = 11;
 // }
 
 (num%2 == 0)? console.log("Even") : console.log("Odd");
+
+//ELSE IF
+var number = -10;
+if(number == 0){
+    console.log("Zero");
+}
+else if(number > 0){
+    console.log("Positive");
+}
+else{
+    console.log("Negative");
+}
+
+(number == 0)? console.log("zero") : (number>0)? console.log("Positive") : console.log("Negative");
