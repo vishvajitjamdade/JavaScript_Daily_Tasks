a127859fd5cc246368b68d5c7ccaa6702047a2e4
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-04-23 15:07:48 +0530

    commit

--- forEach.js
+++ forEach.js
@@ -1,14 +1,27 @@
 let students = ["abc","pqr","lmn","xyz"];
 
+// for(let i = 0;i<students.length;i++){
+//     console.log(students[i]);
+// }
+
 //NOTE : FOR-LOOP is not used in MERN because it takes much time to execute so we use forEach array method
 //       forEach is not a loop It is the array method;
 
-//Using forEach loop
-// for(let student of students){
-//     console.log(student);
-// }
+console.log("forEach Using let-of");
+for(let student of students){
+    console.log(student);
+}
 
-//Using forEach loop with arrow function 
+console.log("Using forEach loop with arrow function");
 students.forEach((student) => {
     console.log(student);
 })
+
+var name = "abcd";
+let i = 0;
+let count = 0;
+while(name.charAt(i)){
+    i++;
+    count++;
+}
+console.log(count);
