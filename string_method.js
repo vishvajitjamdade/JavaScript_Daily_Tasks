eb9601c1d536b6e66d9dc0711eaea4dd5b4704d2
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-05-07 11:40:54 +0530

    word character count

---
+++ string_method.js
@@ -0,0 +1,24 @@
+// var str1 = "welcome";
+// console.log(str1.charAt(0));
+
+// var firstName = "vishvajit";
+// var lastName = "jamdade";
+// console.log(firstName.concat(lastName));
+
+// console.log(str1.indexOf('e'));
+// console.log(str1.search('come'));
+// console.log(str1.match('come'));
+// console.log(str1.repeat(5));
+// console.log(str1.replace('come','l done'));
+// console.log(str1.substring(0,3));
+// console.log(str1.toUpperCase());
+// console.log(str1.toLocaleLowerCase());
+// console.log(str1.split("l"));
+
+
+
+var str2 = "abcdfg";
+var str3 = "xyz";
+
+var ans = str2.substring(0,str2.length/2) + str3 + str2.substring(str2.length/2-1);
+console.log(ans);
