a5580023ec57e04157fe70e4ba50cd852e969e50
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-05-09 22:01:46 +0530

    array split

---
+++ array_split.js
@@ -0,0 +1,8 @@
+let str = "coding";
+let reversed = str.split("").reverse().join("");
+console.log(reversed);
+
+let word = "hello";
+let chars = word.split("");
+chars.reverse();
+console.log(chars.join("-"));
