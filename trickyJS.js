d839df72bba58b4807cd265c1f32ab767601e3df
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-05-12 00:20:51 +0530

    task

---
+++ trickyJS.js
@@ -0,0 +1,7 @@
+console.log(undefined-null);
+console.log(true-false);
+console.log(-true);
+console.log(true+false);
+console.log(-false);
+console.log(typeof undefined);
+console.log(typeof null);
